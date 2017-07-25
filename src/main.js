41e9f757b186806abc250c71a8fda67fcbbb5d1f
Asif Mahmud <amahmud@uci.edu>
2017-07-25 00:15:50 -0700

    added initial app component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-var API = require('./api');
-var chirpStore = require('./stores/chirps');
+var React           = require('react');
+var reactRouter     = require('react-router');
+var Route           = reactRouter.Route;
+var API             = require('./api');
+var chirpStore      = require('./stores/chirps');
+var routes          = (<Route handler={require('./components/App')}></Route>);
+
 
 API.fetchChirps();
+
+reactRouter.run(routes, reactRouter.HistoryLocation, function(Root){
+    React.render(<Root />, document.getElementById('app'));
+});
