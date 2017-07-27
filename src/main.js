53096cb94d8568681a2671107a97e2c3a629e87c
Asif Mahmud <amahmud@uci.edu>
2017-07-26 22:12:53 -0700

    sort chirp list by newest first

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ var API             = require('./api');
 var routes = (
     <Route handler={require('./components/App')}>
         <Route name='home' path='/' handler={require('./components/home')} />
+        <Route name='user' path='/user/:id' handler={require('./components/home')} />
     </Route>
 );
 
