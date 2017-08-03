8769c00e9aa54c558a18231fa009f67e32597771
Asif Mahmud <amahmud@uci.edu>
2017-08-02 23:53:12 -0700

    minor code restructuring

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,27 @@
+/*
+* ----------------------------------------------------
+* Gulp and Browserify
+* ----------------------------------------------------
+* Gulp is used to automate the build process
+* along with Browserify, which allows you to use
+* modules and 'require' statements in the browser.
+*
+*/
+
 var gulp        = require('gulp');
 var browserify  = require('gulp-browserify');
 var concat      = require('gulp-concat');
 var plumber     = require('gulp-plumber');
+
+/*
+* ----------------------------------------------------
+* The 'browserify' task recursively builds all modules
+* specified by the 'require' statements, starting from
+* main.js file located in 'src' directory. It compiles
+* them into a single main.js file and places it into
+* the public folder.
+* ----------------------------------------------------
+*/
 
 gulp.task('browserify', function(){
     gulp.src("src/main.js")
@@ -11,7 +31,21 @@ gulp.task('browserify', function(){
     .pipe(gulp.dest('public'));
 });
 
+/*
+* ----------------------------------------------------
+* The 'default' task just calls the browserify task
+* ----------------------------------------------------
+*/
+
 gulp.task('default', ['browserify']);
+
+/*
+* ----------------------------------------------------
+* The 'watch' task continously watched for any changes
+* made to any files in 'src' directory and re-builds
+* them.
+* ----------------------------------------------------
+*/
 
 gulp.task('watch', function(){
     gulp.watch('src/**/*.*', ['default']);
--- server.js
+++ server.js
@@ -1,12 +1,37 @@
+/*
+* ----------------------------------------------------
+* Express
+* ----------------------------------------------------
+* Express is a web application framework for Node.js.
+* It is used to handle server-side operations, routing,
+* browser sessions etc. For more information, visit:
+* https://github.com/expressjs/express
+*
+*/
+
 var express = require('express');
 var login   = require('./login');
 var chirps  = require('./chirps');
 
 express()
-    .set('view engine', 'ejs')
+    /*
+    * ----------------------------------------------------
+    * Set the view engine to ejs, to let Express know to
+    * use the ejs templating system
+    * ----------------------------------------------------
+    */
+    .set('view engine', 'ejs') /
     .use(express.static('./public'))
     .use(login.routes)
     .use(chirps)
+    /*
+    * ----------------------------------------------------
+    * For any non-static requests, express first checks if
+    * the user is logged in, by calling login.required.
+    * When the user is authenticated, it sends the index
+    * template.
+    * ----------------------------------------------------
+    */
     .get('*', login.required, function(req, res){
         res.render('index', {
             user: login.safe(req.user)
