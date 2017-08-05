8ac8764f9e87b6929faf66f82451454b90bc0c31
Asif Mahmud <amahmud@uci.edu>
2017-08-05 01:18:57 -0700

    update store.js

--- src/stores/chirps.js
+++ src/stores/chirps.js
@@ -1,8 +1,30 @@
+/*
+* ----------------------------------------------------
+* Extends the store module to handle actions specific
+* to chirps.
+* ----------------------------------------------------
+*/
+
+
+
 var constants = require('../constants');
 var UserStore = require('./users');
 
 var ChirpStore = module.exports = require('./store').extend({
+
     init: function(){
+        /*
+        * ----------------------------------------------------
+        * Whenever new chirps enter the system, coming from
+        * the server, the GOT_CHIRPS action will occur. The
+        * chirps store will be listening and call the "set"
+        * function.
+        *
+        * Similarly, Whenever the user creates a chirp, the
+        * CHIRPED action will occur. The chirps store will
+        * add that chirp to the store. 
+        * ----------------------------------------------------
+        */
         this.bind(constants.GOT_CHIRPS, this.set);
         this.bind(constants.CHIRPED, this.add);
     },
