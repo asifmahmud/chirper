d97bcbe2d4eb26f052a74041e67f3039a54c0a7a
Asif Mahmud <amahmud@uci.edu>
2017-09-07 20:23:57 -0700

    Update dispatcher.js

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,13 +1,3 @@
-/*
-* ----------------------------------------------------
-* Flux
-* ----------------------------------------------------
-* Flux is an application architecture, made by Facebook,
-* that is used for building client-side web applications.
-* More information can be found at:
-* https://facebook.github.io/flux/
-*/
-
 var flux = require('flux');
 
 // Inittiate the Flux dispatcher
